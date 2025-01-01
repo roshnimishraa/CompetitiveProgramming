9230654a38af8ef7370c999e89029b64abd03b4b
Roshni Mishra <121380696+roshnimishraa@users.noreply.github.com>
2025-01-01 12:29:06 +0530

    Update Halloumi Boxes.cpp

--- 800/Halloumi Boxes.cpp
+++ 800/Halloumi Boxes.cpp
@@ -1,1 +1,35 @@
-
+#include<iostream> 
+using namespace std;
+#include<bits/stdc++.h>
+#define ll long long
+int main()
+{
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,k;
+        cin>>n>>k;
+        vector<int> a(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+        }
+       bool Sorted = is_sorted(a.begin(), a.end());
+        if(k>=2)
+        {
+            cout<<"YES"<<endl;
+        }
+        else{
+            if(Sorted)
+            {
+                cout<<"YES"<<endl;
+            }
+            else{
+                cout<<"NO"<<endl;
+            }
+        }
+        
+    }
+    return 0;
+}
